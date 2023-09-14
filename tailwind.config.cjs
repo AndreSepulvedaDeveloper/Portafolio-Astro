c09f03f09ceb1faef7b328a8c0af00062e82f383
AndreSepulvedaDeveloper <aasepulveda6@gmail.com>
2023-09-14 11:07:33 -0500

    se sube el repositorio

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      principal: "#64CCC5",
+      secundario: "#EEEEEE",
+      bgcolor: "#0B2447",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
